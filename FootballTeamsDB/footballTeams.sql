d4e3ef9b3e0ce8c92c3f31524940d8e5192ff913
Da4ko <88800194+Da4ko@users.noreply.github.com>
2026-02-22 12:43:49 +0200

    Added more queries

--- FootballTeamsDB/footballTeams.sql
+++ FootballTeamsDB/footballTeams.sql
@@ -145,13 +145,107 @@ LEFT JOIN players ON teams.team_id = players.team_id
 GROUP BY country_name
 ORDER BY count_of_players DESC, country_name
 
+-- You need to move experienced players into the coaches table to act as "Player-Coaches."
+-- Criteria: Find players who are older than 33 and have a dribbling score over 80.
 
+-- Insert Values:
+-- first_name & last_name: From the player.
+-- salary: Set it to exactly $10,000.00 (flat rate).
+-- coach_level: Set it to the player's age minus 30.
 
+INSERT INTO coaches (first_name, last_name, salary, coach_level)
+SELECT first_name, last_name, 10000 as new_salary, age-30 FROM players
+JOIN skills_data on players.skills_data_id = skills_data.skill_data_id
+WHERE age>33 AND dribbling>80;
 
+-- Find coaches who currently train at least one player in a stadium with a capacity greater than 50,000.
+-- Insert Values: Re-insert these same coaches into the coaches table, but append the string ' (Asst)' to their last_name and set their salary to 75% of their current salary.
 
+INSERT INTO coaches(first_name, last_name, salary, coach_level)
+SELECT DISTINCT coaches.first_name, concat(coaches.last_name, " (Asst)") as asst_last_name , coaches.salary*0.75 as asst_salary, coach_level FROM coaches
+JOIN players_coaches ON coaches.coach_id = players_coaches.coach_id
+JOIN players ON players_coaches.player_id = players.player_id
+JOIN teams ON players.team_id = teams.team_id
+JOIN stadiums on teams.stadium_id = stadiums.stadium_id
+WHERE stadiums.capacity > 50000;
 
+-- The league is capping salaries for underperforming teams.
+-- Criteria: Any player who plays for a team established before 1960 and has a shooting score below 50.
+-- Action: Reduce their salary by 10%.
 
+UPDATE players 
+JOIN teams ON players.team_id = teams.team_id
+JOIN skills_data ON players.skills_data_id = skills_data.skill_data_id
+SET salary = salary*0.9
+WHERE established < '1960-01-01' AND shooting < 50;
 
+-- Coaches are being promoted based on the "Pace" of their students.
+-- Criteria: Identify coaches who are training at least one player with a pace over 90.
+-- Action: Increment their coach_level by 2.
 
+UPDATE coaches
+JOIN players_coaches ON coaches.coach_id = players_coaches.coach_id
+JOIN players ON players_coaches.player_id = players.player_id
+JOIN skills_data on players.skills_data_id = skills_data.skill_data_id
+SET coach_level = coach_level + 2 
+WHERE pace > 90;
 
+-- Some players are assigned to teams that point to a stadium_id that doesn't actually exist in the stadiums table.
+-- Goal: Delete players whose team_id belongs to a team with a non-existent stadium_id
+
+DELETE players FROM players 
+JOIN teams ON players.team_id = teams.team_id
+LEFT JOIN stadiums ON teams.stadium_id = stadiums.stadium_id
+WHERE stadiums_id IS NULL;
+
+-- We need to remove skill records that are no longer useful.
+-- Criteria: Delete records from skills_data that are not linked to any player AND have a strength score of exactly 0.
+-- Action: Delete from skills_data.
+
+DELETE skills_data FROM skills_data
+LEFT JOIN players ON players.skills_data_id = skills_data.skill_data_id
+WHERE player_id IS NULL AND strength = 0 ;
+
+-- List the first_name and last_name of the player(s) who have the absolute highest shooting score among all players belonging to a team with more than 1,000,000 fans.
+
+SELECT first_name, last_name FROM players 
+JOIN skills_data ON players.skills_data_id = skills_data.skill_data_id
+JOIN teams ON players.team_id = teams.team_id
+WHERE shooting = (
+	SELECT shooting FROM players 
+	JOIN skills_data ON players.skills_data_id = skills_data.skill_data_id
+	JOIN teams ON players.team_id = teams.team_id
+    WHERE fan_base>1000000
+    ORDER BY shooting DESC
+    LIMIT 1
+) AND fan_base>1000000;
+
+-- List the first_name, last_name, and salary of the coach(es) who are training the absolute maximum number of players.
+
+SELECT coaches.first_name, coaches.last_name, coaches.salary, COUNT(players.player_id) as number_of_players FROM coaches
+JOIN players_coaches ON coaches.coach_id = players_coaches.coach_id
+JOIN players ON players_coaches.player_id = players.player_id
+GROUP BY (coaches.coach_id)
+HAVING number_of_players = (
+ SELECT COUNT(players.player_id) as number_of_players FROM coaches
+ JOIN players_coaches ON coaches.coach_id = players_coaches.coach_id
+ JOIN players ON players_coaches.player_id = players.player_id
+ GROUP BY (coaches.coach_id)
+ ORDER BY number_of_players DESC
+ LIMIT 1
+);
+
+-- The league office wants to know which team has the highest "Payroll." We aren't looking for one player's salary; we want the total sum of all player salaries on a team.
+-- The Goal: List the team_name and the Total Salary of the team(s) that pay the absolute maximum amount of money to their players.
+
+SELECT team_name, SUM(salary) as team_salary FROM teams
+JOIN players ON players.team_id = teams.team_id
+GROUP BY (teams.team_id) 
+HAVING team_salary = (
+SELECT SUM(salary) as team_salary FROM teams
+JOIN players ON players.team_id = teams.team_id
+GROUP BY (teams.team_id) 
+ORDER BY team_salary DESC
+LIMIT 1
+);
 
